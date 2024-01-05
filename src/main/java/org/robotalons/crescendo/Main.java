e8d51404bd3e9dc68a75fa7c5b570118a2759692
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2024-01-04 21:00:46 -0600

    Reorganize File Structure

---
+++ src/main/java/org/robotalons/crescendo/Main.java
@@ -0,0 +1,24 @@
+// ----------------------------------------------------------------[Package]----------------------------------------------------------------//
+package org.robotalons.crescendo;
+// ---------------------------------------------------------------[Libraries]---------------------------------------------------------------//
+import edu.wpi.first.wpilibj.RobotBase;
+// ---------------------------------------------------------------[Main Class]-------------------------------------------------------------//
+/**
+ * 
+ * 
+ * <h1>Main</h1>
+ * 
+ * <p>Robot Project runner class, responsible for robot initialization by starting the Driverstation, CameraServer, and HAL services.</p>
+ * 
+ * @see Robot
+ */
+public final class Main {
+
+  /**
+   * Initializes the robot and underlying systems
+   * @param Options Additional options applied via the command line
+   */
+  public static void main(String... Options) {
+    RobotBase.startRobot(Robot::getInstance);
+  }
+}
