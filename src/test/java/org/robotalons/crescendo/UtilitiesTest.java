fe8d0a7af34b45247d69448650657dcd59fd7435
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2024-01-04 21:28:32 -0600

    Add Unit Testing

---
+++ src/test/java/org/robotalons/crescendo/UtilitiesTest.java
@@ -0,0 +1,44 @@
+// ----------------------------------------------------------------[Package]----------------------------------------------------------------//
+package org.robotalons.crescendo;
+// ---------------------------------------------------------------[Libraries]---------------------------------------------------------------//
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import com.revrobotics.REVLibError;
+import com.ctre.phoenix.ErrorCode;
+import org.junit.jupiter.api.Test;
+// ------------------------------------------------------------[Utility Test Class]---------------------------------------------------------//
+public final class UtilitiesTest {
+
+  /**
+   * Test CTRE Error Codes throw runtime expected exceptions 
+   */
+  @Test
+  void CheckErrorCTRE() {
+    assertThrows((RuntimeException.class), () -> CheckCTRECode(ErrorCode.GeneralError));
+    assertThrows((RuntimeException.class), () -> CheckCTRECode(ErrorCode.FirmVersionCouldNotBeRetrieved));
+    assertDoesNotThrow(() -> CheckCTRECode(ErrorCode.OK));
+  }
+
+  static void CheckCTRECode(final ErrorCode errorCode) {
+      if (errorCode != ErrorCode.OK) {
+        throw new RuntimeException(String.format("%s: %s%n", "", errorCode.toString()));
+        
+    }
+  }
+
+  /**
+   * Test REVLib Error Codes throw runtime expected exceptions
+   */
+  @Test
+  void CheckErrorREV() {
+    assertThrows((RuntimeException.class), () -> CheckREVLibCode(REVLibError.kError));
+    assertThrows((RuntimeException.class), () -> CheckREVLibCode(REVLibError.kCantFindFirmware));
+    assertDoesNotThrow(() -> CheckREVLibCode(REVLibError.kOk));
+  }
+
+  static void CheckREVLibCode(final REVLibError error) {
+    if (error != REVLibError.kOk) {
+        throw new RuntimeException(String.format("%s: %s", "", error.toString()));
+    }
+  }
+}
